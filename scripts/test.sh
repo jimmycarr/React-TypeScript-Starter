8e8c6f9ca229fd703294bfb14c5dc131d2a9b0a5
James Carr <pair+james@garage>
2018-10-19 14:22:08 +0100

    Added scripts for pipeline stages

---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Invoke tests here
+
+curl -O https://nodejs.org/dist/v8.9.3/node-v8.9.3-linux-x64.tar.xz
+tar -xf node-v8.9.3-linux-x64.tar.xz
+ls node-v8.9.3-linux-x64/bin
+export PATH=${PWD}/node-v8.9.3-linux-x64/bin:$PATH
+
+npm test
